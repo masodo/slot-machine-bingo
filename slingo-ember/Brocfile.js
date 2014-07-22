940e20f39eebf90f24ebc09731a92f9ba1496ca1
Shaun Butler <shaun.butler@gmail.com>
2014-07-22 10:29:21 -0400

    Font awesome installed in cli version

--- slingo-ember/Brocfile.js
+++ slingo-ember/Brocfile.js
@@ -1,8 +1,24 @@
-/* global require, module */
-
+var pickFiles = require('broccoli-static-compiler');
+var mergeTrees = require('broccoli-merge-trees');
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp();
-app.import('vendor/bootstrap/dist/css/bootstrap.css');
+var app = new EmberApp({
+  name: require('./package.json').name,
 
-module.exports = app.toTree();
+  minifyCSS: {
+    enabled: true,
+    options: {}
+  },
+
+  getEnvJSON: require('./config/environment')
+});
+
+// https://github.com/stefanpenner/ember-cli/issues/777
+var fontTree = pickFiles('vendor/fontawesome/fonts', {
+  srcDir: '/',
+  files: ['fontawesome-webfont.eot','fontawesome-webfont.ttf','fontawesome-webfont.svg','fontawesome-webfont.woff'],
+  destDir: '/assets/fonts'
+});
+
+app.import('vendor/bootstrap/dist/css/bootstrap.css');
+module.exports = mergeTrees([app.toTree(), fontTree]);
