fb926e9f4510d62e3188e919ab5c65bfd0884b10
Shaun Butler <shaun.butler@gmail.com>
2014-08-04 12:46:09 -0400

    Add computed properties for cells
    # 50-character subject line
    #
    # 72-character wrapped longer description. This should answer:
    #
    # * Why was this change necessary?
    # * How does it address the problem?
    # * Are there any side effects?
    #
    # Include a link to the ticket, if any.

--- slingo-ember/app/models/cell.js
+++ slingo-ember/app/models/cell.js
@@ -1,4 +1,19 @@
 import Ember from "ember";
 
 export default Ember.Object.extend({
+  isJoker: function() {
+    return (this.get('icon') === 'fa fa-github');
+  }.property('icon'),
+
+  isCoin: function() {
+    return (this.get('icon') === 'fa fa-usd');
+  }.property('icon'),
+
+  isDevil: function() {
+    return (this.get('icon') === 'fa fa-reddit');
+  }.property('icon'),
+
+  isCovered: function() {
+    return (this.get('value') === 'X');
+  }.property('value')
 });
