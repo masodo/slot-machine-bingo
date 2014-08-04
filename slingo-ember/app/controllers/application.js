43d8d2f2fc87b7222efd0d81c7d3b16fec681e12
Shaun Butler <shaun.butler@gmail.com>
2014-08-04 18:36:47 -0400

    Adjust score for devil and coin
    * Also allow joker to cover any cell in that column

--- slingo-ember/app/controllers/application.js
+++ slingo-ember/app/controllers/application.js
@@ -1,22 +1,38 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
-  cells: Ember.computed.alias('content.board.cells'),
+  cells: Ember.computed.alias('content.board.cellMatrix'),
   spinner: Ember.computed.alias('content.board.spinner'),
+  score: Ember.computed.alias('content.score'),
+  horizontalComplete: Ember.computed.alias('content.board.horizontalComplete'),
+  verticalComplete: Ember.computed.alias('content.board.verticalComplete'),
   actions: {
     newGameClick: function() {
       this.get('content').newGame();
     },
     cellClick: function(cell) {
-      if (this.get('spinner').indexOf(cell.get('value')) > -1) {
+      if (this.get('spinner')[cell.get('col')].get('value') === cell.get('value') || this.get('spinner')[cell.get('col')].get('isJoker')) {
         cell.set('value', 'X');
-        this.get('board').incrementProperty('covered');
+        this.get('content').addScoreValue(200);
       }
     },
     spinClick: function() {
+      var _this = this;
       if (this.get('spins') < this.get('maxSpins')) {
         this.incrementProperty('spins');
         this.get('board').spin();
+        this.get('spinner').forEach( function(cell) {
+
+          if(cell.get('isCoin')) {
+            _this.get('content').addScoreValue(1000);
+          }
+
+          if(cell.get('isDevil')) {
+            var score = _this.get('content.score');
+            var penalty = score * -1 / 2;
+            _this.get('content').addScoreValue(penalty);
+          }
+        });
       }
     }
   }
